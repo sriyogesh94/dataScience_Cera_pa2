5bc7fb5fce6a2bdba0debef9439697fdb6ef52e8
sriyogesh94 <sriyogesh94@gmail.com>
2016-07-26 00:49:17 +0530

    Implemented the entire function

--- best_hospital.R
+++ best_hospital.R
@@ -1,6 +1,10 @@
 best <- function(statev, outcomev){
     
-    outcome.data <- read.csv("outcome-of-care-measures.csv")
+    #Reads Data
+    
+    outcome.data <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = FALSE)
+    
+    #Terminates program for ivalid arguments
     
     if(!(statev %in% outcome.data$State)){
         
@@ -13,6 +17,54 @@ best <- function(statev, outcomev){
         stop("invalid outcome")
     }
     
-    "success"
-
+    #Extracts required columns for analysis and renames the columns
+    
+    processed.data <- outcome.data[ , c(2, 7, 11, 17, 23)]
+    
+    colnames(processed.data) <- c("hospital", "state", "attack", "failure", "pneumonia")
+    
+    splitData <- split(processed.data, processed.data$state)
+    
+    split.state <- as.data.frame(splitData[statev])
+    
+    colnames(split.state) <- c("hospital", "state", "attack", "failure", "pneumonia")
+    
+    #Collects hospitals with lowest mortality in the specified outcome
+    
+    if(outcomev == "heart attack"){
+        
+        lowest.mortality <- min(split.state$attack, na.rm = TRUE)
+        
+        lowest.mortality <- which(split.state$attack == lowest.mortality)
+        
+        best.hospital <- split.state$hospital[lowest.mortality]
+    }
+    
+    else if(outcomev == "heart failure"){
+        
+        lowest.mortality <- min(split.state$failure, na.rm = TRUE)
+        
+        lowest.mortality <- which(split.state$failure == lowest.mortality)
+        
+        best.hospital <- split.state$hospital[lowest.mortality]
+        
+    }
+    
+    else if(outcomev == "pneumonia"){
+        
+        lowest.mortality <- min(split.state$pneumonia, na.rm = TRUE)
+        
+        lowest.mortality <- which(split.state$pneumonia == lowest.mortality)
+        
+        best.hospital <- split.state$hospital[lowest.mortality]
+        
+    }
+    
+    
+    
+    #Returns the best hospital sorted in alphabetical order
+    best.hospital <- sort(as.vector(best.hospital))[1]
+    
+    best.hospital
+    
 }
